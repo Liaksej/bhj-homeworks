30314c92b33a849f6d289e2ea811a425752680ce
Liaksej <tarusov.liaksej@gmail.com>
2023-03-27 19:56:59 +0200

    Make close functionality of popups universal and change popups by click on button without layering

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,18 +2,26 @@ const bannerOne = document.getElementById("modal_main");
 const closeBtn = document.getElementsByClassName(
   "modal__close modal__close_times"
 );
-const showSuccess = document.getElementsByClassName(
-  "btn btn_danger modal__close show-success"
-)[0];
-const bannerTwo = document.getElementById("modal_success");
+const showSuccess = document.getElementsByClassName("btn");
 
 bannerOne.classList.add("modal_active");
-showSuccess.onclick = () => {
-  bannerTwo.classList.add("modal_active");
-};
-closeBtn[0].onclick = () => {
-  bannerOne.classList.remove("modal_active");
-};
-closeBtn[1].onclick = () => {
-  bannerTwo.classList.remove("modal_active");
-};
+
+for (const showSuccessItem of showSuccess) {
+  showSuccessItem.onclick = function () {
+    if (this.closest(".modal").classList.contains("modal_active")) {
+      this.closest(".modal").classList.remove("modal_active");
+    }
+    if (this.closest("#modal_main")) {
+      document.getElementById("modal_success").classList.add("modal_active");
+    }
+  };
+}
+for (const closeBtnElement of closeBtn) {
+  closeBtnElement.onclick = function () {
+    if (this.closest("#modal_main")) {
+      this.closest("#modal_main").classList.remove("modal_active");
+    } else if (this.closest("#modal_success")) {
+      this.closest("#modal_success").classList.remove("modal_active");
+    }
+  };
+}

87cc43ddf534d8e842272da62463a4e8a68cfc6d
Liaksej <tarusov.liaksej@gmail.com>
2023-04-10 23:16:31 +0200

    Fix adding task with only spaces string

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,43 +1,43 @@
 const inputPhrase = document.getElementById("task__input");
 
 function createTask(event) {
-  const task = document.createElement("div");
-  task.classList.add("task");
+  if (event.value.length > 0 && event.value.match(/\w/)) {
+    const task = document.createElement("div");
+    task.classList.add("task");
 
-  const taskTitle = document.createElement("div");
-  taskTitle.classList.add("task__title");
-  taskTitle.textContent = event.value;
+    const taskTitle = document.createElement("div");
+    taskTitle.classList.add("task__title");
+    taskTitle.textContent = event.value;
 
-  const taskRemove = document.createElement("a");
-  taskRemove.classList.add("task__remove");
-  taskRemove.href = "#";
-  taskRemove.textContent = "x";
+    const taskRemove = document.createElement("a");
+    taskRemove.classList.add("task__remove");
+    taskRemove.href = "#";
+    taskRemove.textContent = "x";
 
-  task.appendChild(taskTitle);
-  task.appendChild(taskRemove);
+    task.appendChild(taskTitle);
+    task.appendChild(taskRemove);
 
-  const taskContainer = document.getElementById("tasks__list");
+    const taskContainer = document.getElementById("tasks__list");
 
-  if (taskContainer.children.length > 0) {
-    taskContainer.insertBefore(task, taskContainer.firstElementChild);
-  } else {
-    taskContainer.appendChild(task);
+    if (taskContainer.children.length > 0) {
+      taskContainer.insertBefore(task, taskContainer.firstElementChild);
+    } else {
+      taskContainer.appendChild(task);
+    }
+    event.value = "";
   }
-  event.value = "";
 }
 
 function doIfPrompt(event) {
   event.target.addEventListener("keydown", (event) => {
-    if (event.code === "Enter" && event.target.value.length > 0) {
+    if (event.code === "Enter") {
       event.preventDefault();
       createTask(inputPhrase);
     }
   });
   document.getElementById("tasks__add").addEventListener("click", (event) => {
-    if (inputPhrase.value.length > 0) {
-      event.preventDefault();
-      createTask(inputPhrase);
-    }
+    event.preventDefault();
+    createTask(inputPhrase);
   });
 }
 

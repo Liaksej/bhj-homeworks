6d81cd07b6e018d1d6eac7e40d46bfdb369934fe
Liaksej <tarusov.liaksej@gmail.com>
2023-04-12 14:54:49 +0200

    fix: Correct functionality of ckeckboxes and optimize code

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,153 +1,44 @@
 const mainInterest = document.querySelector(".interests_main");
 
-function groupChangeChildren(event) {
-  // Проставляет и убирает indeterminate, а также добавляет checked, если внутренние вложенные элементы checked
-  function checkIndetermination() {
-    {
-      const lengthOfAllCkeckboxes = event.target
-        .closest("ul")
-        .querySelectorAll(".interest__check").length;
-      const lengthOfCheckedCheckboxes = event.target
-        .closest("ul")
-        .querySelectorAll(".interest__check:checked").length;
-      let targetRecursion = event.target;
-      if (
-        lengthOfAllCkeckboxes > lengthOfCheckedCheckboxes &&
-        lengthOfCheckedCheckboxes !== 0
-      ) {
-        // Рекурсивно проставляет indeterminate у родителей
-        function recursionUpdate(targetRecursion) {
-          if (
-            targetRecursion
-              .closest("ul")
-              .parentElement.classList.contains("interests_main")
-          ) {
-            return targetRecursion;
-          } else {
-            let newTarget = targetRecursion
-              .closest("ul")
-              .parentElement.querySelector(".interest__check");
+function checkIndetermination(event) {
+  const targetCheckbox = event.closest(".interests_active");
 
-            let result = recursionUpdate(newTarget);
-            result.indeterminate = true;
-            return targetRecursion;
-          }
-        }
+  if (!targetCheckbox) return;
 
-        recursionUpdate(targetRecursion);
-      } else if (lengthOfAllCkeckboxes === lengthOfCheckedCheckboxes) {
-        // Рекурсивно изменяет indeterminate у родителей на checked
-        function recursionChange(targetRecursion) {
-          if (
-            targetRecursion
-              .closest("ul")
-              .parentElement.classList.contains("interests_main")
-          ) {
-            return targetRecursion;
-          } else {
-            let newTarget = targetRecursion
-              .closest("ul")
-              .parentElement.querySelector(".interest__check");
+  const parentCheckbox = targetCheckbox
+    .closest(".interest")
+    .querySelector(".interest__check");
+  const childCheckboxes = Array.from(
+    targetCheckbox.querySelectorAll(".interest__check")
+  );
 
-            let result = recursionChange(newTarget);
-            result.checked = true;
-            result.indeterminate = false;
-            return targetRecursion;
-          }
-        }
-
-        recursionChange(targetRecursion);
-      } else if (lengthOfCheckedCheckboxes === 0) {
-        // Рекурсивно убирает indeterminate и checked у родителей
-        function recursionDiscard(targetRecursion) {
-          if (
-            targetRecursion
-              .closest("ul")
-              .parentElement.classList.contains("interests_main")
-          ) {
-            return targetRecursion;
-          } else {
-            let newTarget = targetRecursion
-              .closest("ul")
-              .parentElement.querySelector(".interest__check");
-
-            let result = recursionDiscard(newTarget);
-            if (
-              result.closest("ul").querySelectorAll(".interest__check:checked")
-                .length === 0
-            ) {
-              result.indeterminate = false;
-            }
-            result.checked = false;
-            return targetRecursion;
-          }
-        }
-
-        recursionDiscard(targetRecursion);
-
-        event.target
-          .closest("ul")
-          .parentElement.querySelector(".interest__check").checked = false;
-        event.target
-          .closest("ul")
-          .parentElement.querySelector(
-            ".interest__check"
-          ).indeterminate = false;
-      } else if (
-        lengthOfCheckedCheckboxes === 0 &&
-        Array.from(
-          event.target.closest("ul").querySelectorAll(".interest__check")
-        ).find((element) => element.indeterminate === true)
-      ) {
-      }
-    }
+  if (childCheckboxes.every((item) => item.checked)) {
+    parentCheckbox.checked = true;
+    parentCheckbox.indeterminate = false;
+  } else if (
+    childCheckboxes.some((item) => item.checked || item.indeterminate)
+  ) {
+    parentCheckbox.indeterminate = true;
+  } else {
+    parentCheckbox.checked = false;
+    parentCheckbox.indeterminate = false;
   }
 
-  if (
-    Array.from(
-      mainInterest.querySelectorAll(".interest__check:checked")
-    ).includes(event.target) &&
-    (event.target
-      .closest("ul")
-      .parentElement.classList.contains("interests_main") ||
-      event.target.closest("ul").parentElement.classList.contains("interest"))
-  ) {
+  checkIndetermination(parentCheckbox);
+}
+
+function groupChangeChildren(event) {
+  if (event.target.matches(".interest__check")) {
     Array.from(
       event.target
         .closest("li.interest")
         .querySelectorAll("input.interest__check")
     ).forEach((element) => {
-      element.checked = true;
-      element.indeterminate = false;
+      element.checked = event.target.checked;
     });
-  } else if (
-    event.target.classList.contains("interest__check") &&
-    event.target.checked === false &&
-    (event.target
-      .closest("ul")
-      .parentElement.classList.contains("interests_main") ||
-      event.target.closest("ul").parentElement.classList.contains("interest"))
-  ) {
-    Array.from(
-      event.target
-        .closest("li.interest")
-        .querySelectorAll("input.interest__check")
-    ).forEach((element) => (element.checked = false));
   }
 
-  if (
-    Array.from(
-      mainInterest.querySelectorAll(".interest__check:checked")
-    ).includes(event.target) &&
-    event.target.closest("ul").classList.contains("interests_active")
-  ) {
-    checkIndetermination();
-  } else if (
-    event.target.classList.contains("interest__check") &&
-    event.target.checked === false
-  ) {
-    checkIndetermination();
-  }
+  checkIndetermination(event.target);
 }
 
 mainInterest.addEventListener("change", groupChangeChildren);

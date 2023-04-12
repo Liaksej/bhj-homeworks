372f7ced28ee042bd7f40ee124eb368a3bb30ffe
Liaksej <tarusov.liaksej@gmail.com>
2023-04-12 22:54:35 +0200

    fix: Tooltip closes when you click again a link

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,9 +1,16 @@
 function toggleTooltip(element) {
-  if (document.querySelector(".tooltip")) {
-    document.querySelector(".tooltip").remove();
-  }
+  element.preventDefault();
   if (element.target.classList.contains("has-tooltip")) {
-    element.preventDefault();
+    if (document.querySelector(".tooltip")) {
+      if (
+        document.querySelector(".tooltip").textContent === element.target.title
+      ) {
+        document.querySelector(".tooltip").remove();
+        return;
+      }
+      document.querySelector(".tooltip").remove();
+    }
+
     const tooltip = document.createElement("div");
     tooltip.classList.add("tooltip", "tooltip_active");
     tooltip.textContent = element.target.title;
